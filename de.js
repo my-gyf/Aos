11957f941bbb94d46c1ea8b24b9ebd7b58f59414
my-gyf <81071919+my-gyf@users.noreply.github.com>
2023-03-05 13:53:12 +0800

    Add files via upload

---
+++ de.js
@@ -0,0 +1,17 @@
+
+function check(thisform) {
+ 
+	var name=document.getElementById("name").value;  //读取表单数据，创建变量
+	var pass=document.getElementById("pass").value;
+ 
+	if (name=="admin" && pass=="123456" || name=="123456" && pass=="234567") {  
+        //验证变量。此处设置账号、密码（可设置多组，用||隔开）
+		alert("登录成功！");
+		window.document.f.action="start.html";  //此处设置登录后跳转页面
+		window.document.f.submit();
+	}
+	else{
+		alert("用户名或密码错误！");
+	}
+ 
+}
