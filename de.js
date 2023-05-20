4fc9af987c2df55d56d18e64b8b94fbdff7ba393
my-gyf <81071919+my-gyf@users.noreply.github.com>
2023-05-20 21:27:27 +0800

    Update de.js

--- de.js
+++ de.js
@@ -4,7 +4,7 @@ function check(thisform) {
 	var name=document.getElementById("name").value;  //读取表单数据，创建变量
 	var pass=document.getElementById("pass").value;
  
-	if (name=="刘溢采" && pass=="123456" || name=="Caumtcs" && pass=="111111") {  
+	if (name=="刘溢采" && pass=="123456" || name=="Caumtcs" && pass=="111111" || name=="陈佳慧" && pass=="123456") {  
         //验证变量。此处设置账号、密码（可设置多组，用||隔开）
 		alert("登录成功！");
 		window.document.f.action="start.html";  //此处设置登录后跳转页面
